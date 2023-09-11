e0646aa9a2e14216783c58ce2c7700f781f88cf1
Emelie Pozzi <emelie.pozzi@iths.se>
2023-09-11 09:37:42 +0200

    testar igen

--- EmeliesTestMapp/TestarGitHub.java
+++ EmeliesTestMapp/TestarGitHub.java
@@ -1,6 +1,8 @@
 public class TestarGitHub{
 
     public static void main(String[] args) {
+
+        System.out.println("Hello detta är min test för att stagea och commita till github.");
         
     }
 
